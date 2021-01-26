95b11206410c7b7bc3bfcdd35f865c1ab0a0a9c9
Keyur996 <61685652+Keyur996@users.noreply.github.com>
2021-01-26 23:40:54 +0530

    Keyur Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,121 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Browser Animation Module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Prime Ng modules
+// Forms Modules
+import {CheckboxModule} from 'primeng/checkbox';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import {ChipsModule} from 'primeng/chips';
+import {DropdownModule} from 'primeng/dropdown'; // Angular Scrolling Module
+import {InputMaskModule} from 'primeng/inputmask';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {KnobModule} from 'primeng/knob';
+import {KeyFilterModule} from 'primeng/keyfilter';
+import {ListboxModule} from 'primeng/listbox';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {PasswordModule} from 'primeng/password';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {RatingModule} from 'primeng/rating';
+import {SliderModule} from 'primeng/slider';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {ToggleButtonModule} from 'primeng/togglebutton';
+import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
+
+//Button Modules Prime Ng
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+
+//Data View Modules Primeng
+import {DataViewModule} from 'primeng/dataview';
+import {FullCalendarModule} from 'primeng/fullcalendar';
+import {GMapModule} from 'primeng/gmap';
+import {OrderListModule} from 'primeng/orderlist';
+import {OrganizationChartModule} from 'primeng/organizationchart';
+import {PaginatorModule} from 'primeng/paginator';
+import {PickListModule} from 'primeng/picklist';
+import {TableModule} from 'primeng/table';
+import {TimelineModule} from 'primeng/timeline';
+import {TreeModule} from 'primeng/tree';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
+
+//Menu PrimeNg
+import {MenuModule} from 'primeng/menu'; //required when using MegaMenu
+import {BreadcrumbModule} from 'primeng/breadcrumb';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {MegaMenuModule} from 'primeng/megamenu';
+import {MenubarModule} from 'primeng/menubar';
+import {PanelMenuModule} from 'primeng/panelmenu';
+import {SlideMenuModule} from 'primeng/slidemenu';
+import {StepsModule} from 'primeng/steps';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {TieredMenuModule} from 'primeng/tieredmenu';
+
+//file and http Module
+import {FileUploadModule} from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
+
+//Toastr Module
+import { ToastrModule } from 'ngx-toastr';
+import { ToastModule } from 'primeng/toast';
+
+//Alert Button
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmPopupModule} from 'primeng/confirmpopup';
+import {DialogModule} from 'primeng/dialog';
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
+import {OverlayPanelModule} from 'primeng/overlaypanel';
+import {SidebarModule} from 'primeng/sidebar';
+import {TooltipModule} from 'primeng/tooltip';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { AddClientComponent } from './pages/add-client/add-client.component';
+import { EditClientComponent } from './pages/edit-client/edit-client.component';
+import { ClientDetailComponent } from './pages/client-detail/client-detail.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundComponent } from './layouts/not-found/not-found.component';
+// Forms Modules
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    ClientsComponent,
+    SidebarComponent,
+    AddClientComponent,
+    EditClientComponent,
+    ClientDetailComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, CheckboxModule, AutoCompleteModule, CalendarModule, ChipsModule, DropdownModule,
+    InputMaskModule, InputNumberModule, InputSwitchModule, InputTextModule, InputTextareaModule, KnobModule,
+    KeyFilterModule, ListboxModule, MultiSelectModule, PasswordModule, RadioButtonModule, RatingModule, SliderModule, SelectButtonModule, ToggleButtonModule, TriStateCheckboxModule, ButtonModule, SplitButtonModule, FileUploadModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      closeButton: true,
+    }),
+    ToastModule, TableModule, DataViewModule, FullCalendarModule, GMapModule, OrderListModule, OrganizationChartModule, PaginatorModule, PickListModule, TimelineModule, TreeModule, VirtualScrollerModule, ConfirmDialogModule, ConfirmPopupModule, DialogModule, DynamicDialogModule, OverlayPanelModule, SidebarModule, TooltipModule, MenuModule, BreadcrumbModule, ContextMenuModule, MegaMenuModule, MenubarModule, SidebarModule, TabMenuModule, PanelMenuModule, SlideMenuModule, StepsModule, TieredMenuModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
